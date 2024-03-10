8ebbdb323cbbbcb8d4c0fd4851fe0ae51f298ead
didges <didges73@gmail.com>
2024-03-10 18:09:17 +0300

    ODD_PRINT added using func in main

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-from math import sum_plus_two
+from math import sum_plus_two, is_odd
 
 print("Hello world!")
 print(sum_plus_two(1, 4))
+print(is_odd(5))
+
+for i in range(10):
+    print(f"{i}^2={i**2}")
+
+print("Thx for using")
